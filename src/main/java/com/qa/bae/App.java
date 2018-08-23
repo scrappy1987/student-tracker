0d421c484e97b7da0da63f5a1dee02275fd6f722
scrappy1987 <jagordon87@msn.com>
2018-08-23 10:00:36 +0100

    second commit

--- src/main/java/com/qa/bae/App.java
+++ src/main/java/com/qa/bae/App.java
@@ -1,9 +1,14 @@
 package com.qa.bae;
 
+import com.qa.bae.domain.Student;
+import com.qa.bae.service.StudentService;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello BAE");
+		StudentService service = new StudentService();
+		Student tony = new Student("Tony", "Glover");
+		service.addStudentToMap(tony);
 
 	}
 
